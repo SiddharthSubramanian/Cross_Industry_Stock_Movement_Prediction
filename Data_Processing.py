a8a8273594f3dd95cf4a818c1c7e12a3262ed796
Siddharth Subramanian <siddhu.1992.subramanian@gmail.com>
2021-07-04 16:17:26 +0100

    Update Data_Processing.py
    Reshape data and train_test_split added

--- Data_Processing.py
+++ Data_Processing.py
@@ -41,3 +41,29 @@ def data_processing(dict_data_in,dict_data_out):
 
 ## transforming the data for 1st step
 in_data,out_data = data_processing(Metal_data ,Auto_data)
+
+X = pd.DataFrame()
+for k in in_data.keys():
+  X[k + "_%_change_delivery"] = in_data[k]['%_change_delivery']
+  X[k + "_%_change_price"] = in_data[k]['%_change_price']
+
+Y = pd.DataFrame()
+for k in out_data.keys():
+  Y[k + "_%_change_delivery"] = out_data[k]['%_change_delivery']
+  Y[k + "_%_change_price"] = out_data[k]['%_change_price']
+timestep = 14
+X_data = []
+Y_data = []
+for i in range(len(X)- timestep -1):
+    X_data.append(X.iloc[i:(i+timestep)])
+    Y_data.append(np.array(Y.iloc[i+timestep]))
+split = int(np.ceil(len(X_data)*0.8))
+train_X, train_Y = X_data[:split],Y_data[:split]
+test_X, test_Y = X_data[split:],Y_data[split:]
+train_X = np.array(train_X)
+train_Y = np.array(train_Y)
+
+test_X = np.array(test_X)
+test_Y = np.array(test_Y)
+train_Y = train_Y.reshape(665,1,26)
+test_Y = test_Y.reshape(166,1,26)
